8d6b22e15b0e3e28f6d3235f497dda6bb69f7207
Ryan Epis <louisedepone@gmail.com>
2024-02-29 22:51:32 +0800

    add about me

--- src/Components/Aboutme.jsx
+++ src/Components/Aboutme.jsx
@@ -6,23 +6,23 @@ export default function
     <div className='relative w-screen  my-20 flex md:flex-row flex-col gap-8 justify-center items-center md:gap-[16rem]' id='aboutme' >
     <div className='flex  flex-col md:w-[50%] justify-center items-center gap-8 '>
         <div className='aboutmecarc rounded-md p-5'>
-            Creative Side
+             <div className='my-2 font-bold'>Creative Side</div>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum deleniti suscipit libero, eos doloremque natus! Natus quibusdam exercitationem assumenda eaque accusamus at in praesentium quam aperiam. Nemo tempora fugit ipsa!</p>
         
         </div>
         <div className='aboutmecarc rounded-md p-5'>
-            Music Side
+        <div className='my-2 font-bold'> Music Side</div>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum deleniti suscipit libero, eos doloremque natus! Natus quibusdam exercitationem assumenda eaque accusamus at in praesentium quam aperiam. Nemo tempora fugit ipsa!</p>
         </div>
     </div>
     <div className='flex  flex-col md:w-[50%] justify-center items-center gap-8'>
         <div className='aboutmecarc rounded-md p-5'>
-            Student Side
+           <div className='my-2 font-bold'> Student Side</div>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum deleniti suscipit libero, eos doloremque natus! Natus quibusdam exercitationem assumenda eaque accusamus at in praesentium quam aperiam. Nemo tempora fugit ipsa!</p>
         
         </div>
         <div className='aboutmecarc rounded-md p-5'>
-            Developer Side
+            <div className='my-2 font-bold'>Developer Side</div>
         
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum deleniti suscipit libero, eos doloremque natus! Natus quibusdam exercitationem assumenda eaque accusamus at in praesentium quam aperiam. Nemo tempora fugit ipsa!</p>
         </div>
